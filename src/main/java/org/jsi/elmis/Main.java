fe721737c87392e2f85a9d951024391927d58226
Mekbib Awoke <pmdtot@gmail.com>
2019-02-04 17:34:54 +0200

    rename captions

--- src/main/java/org/jsi/elmis/Main.java
+++ src/main/java/org/jsi/elmis/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/Main.fxml"));
 
-        primaryStage.setTitle("Facility Edition Product Standardization");
+        primaryStage.setTitle("eLMIS FE | Offline Synchronization tool");
         primaryStage.setScene(new Scene(root, 400, 300));
         primaryStage.setOnCloseRequest(windowEvent -> {
             System.exit(0);
